1be46ab72beb0e47fb074dbe1e503252a8d3ba4d
Colin Lea <colincsl@gmail.com>
2012-10-15 20:21:31 -0400

    Netbook crash update

--- pyKinectTools/utils/RealtimeReader.py
+++ pyKinectTools/utils/RealtimeReader.py
@@ -1,110 +1,105 @@
 
-
 from pyKinectTools.algs.BackgroundSubtraction import *
 from pyKinectTools.utils.DepthUtils import *
 from openni import *
 import time
 
 class RealTimeDevice:
-	ctx = []
-	depth = []
-	depthIm = []
-	depthIm8 = []
-	color = []	
-	colorIm = []	
+        ctx = []
+        depth = []
+        depthIm = []
+        depthIm8 = []
+        color = []
+        colorIm = []
 
-	constrain = []
-	maxDist = np.inf
+        constrain = []
+        maxDist = np.inf
 
-	bgModel = []
-	bgModel8 = []
+        bgModel = []
+        bgModel8 = []
 
-	def __init__(self, ctx=[]):
-		if ctx != []:
-			self.ctx = ctx
-		else:
-			self.ctx = Context()
-			self.ctx.init()
-			print "New context created for depth device."			
+        def __init__(self, ctx=[]):
+                if ctx != []:
+                        self.ctx = ctx
+                else:
+                        self.ctx = Context()
+                        self.ctx.init()
+                        print "New context created for depth device."
 
-	def addDepth(self, constrain=[500, 2000]):
-		self.constrain = constrain
+        def addDepth(self, constrain=[500, 2000]):
+                self.constrain = constrain
+                self.maxDist = constrain[1]
 
-		try:
-			self.depth = DepthGenerator()
-			self.depth.create(self.ctx)
-			self.depth.set_resolution_preset(RES_VGA)
-			self.depth.fps = 30			
-		except:
-			print "Depth module can not load."
-
-
-		if self.color != []:
-			self.depth.alternative_view_point_cap.set_view_point(depthDevice.color)
-
-	def addColor(self):
-
-		try:
-			self.color = ImageGenerator()
-			self.color.create(self.ctx)
-			self.color.set_resolution_preset(RES_VGA)
-			self.color.fps = 30			
-		except:
-			print "Color module can not load."			
-
-		if self.depth != []:
-			self.depth.alternative_view_point_cap.set_view_point(self.color)
-
-	def setMaxDist(self, dist):
-		pass
-
-	def start(self):
-		self.ctx.start_generating_all()
-
-	def stop(self):
-		self.ctx.shutdown()
-
-	def update(self):
-		ret = self.ctx.wait_any_update_all()
-		assert ret == None, "Error updating depth device."
-
-		if self.depth != []:
-			depthRawT = self.depth.get_tuple_depth_map()
-			im = np.array(depthRawT).reshape([self.depth.res[1],self.depth.res[0]])
-
-			im[im > self.maxDist] = 0
-			self.depthIm = im
-			self.depthIm8 = constrain(im, self.constrain[0], self.constrain[1])
-
-		if self.color != []:
-			# colorRawT = self.color.get_tuple_image_map()
-			# self.colorIm = np.array(colorRawT, dtype=np.uint8).reshape([self.color.res[1],self.color.res[0], 3])
-
-			colorRawString = self.color.get_synced_image_map()
-			colorRaw = np.fromstring(colorRawString, dtype=np.uint8)
-			self.colorIm = np.array(colorRaw, dtype=np.uint8).reshape([self.color.res[1],self.color.res[0], 3])
+                try:
+                        self.depth = DepthGenerator()
+                        self.depth.create(self.ctx)
+                        #self.depth.set_resolution_preset(RES_QVGA)
+                        #self.depth.fps = 10                    
+                except:
+                        print "Depth module can not load."
 
 
-	def generateBackgroundModel(self):
-		# Get set of 5 frames and create background model
-		depthImgs = []
-		depthStackInd = 0
-		for i in xrange(5):
-			ret = self.ctx.wait_one_update_all(self.depth)
-			assert ret == None, "Error getting depth map"
+                if self.color != []:
+                        self.depth.alternative_view_point_cap.set_view_point(depthDevice.color)
 
-			depthRawT = self.depth.get_tuple_depth_map()
-			im = np.array(depthRawT).reshape([self.depth.res[1],self.depth.res[0]])
-			depthImgs.append(im)
-			time.sleep(.2)
+        def addColor(self):
 
-		depthImgs = np.dstack(depthImgs)
+                try:
+                        self.color = ImageGenerator()
+                        self.color.create(self.ctx)
+                        #self.color.set_resolution_preset(RES_QVGA)
+                        #self.color.fps = 10                    
+                except:
+                        print "Color module can not load."
 
-		self.bgModel = getMeanImage(depthImgs)
+                if self.depth != []:
+                        self.depth.alternative_view_point_cap.set_view_point(self.color)
 
-		self.bgModel8 = constrain(self.bgModel, self.constrain[0], self.constrain[1])
-		self.bgModel8[self.bgModel8==self.bgModel8.max()] = 0
+        def setMaxDist(self, dist):
+                self.maxDist = dist
 
-		# return self.depthIm
+        def start(self):
+                self.ctx.start_generating_all()
+
+        def stop(self):
+                self.ctx.shutdown()
+
+        def update(self):
+                ret = self.ctx.wait_any_update_all()
+                assert ret == None, "Error updating depth device."
+
+                if self.depth != []:
+                        #if self.depthIm != []:
+                        #       del self.depthIm
+                        self.depthIm = np.frombuffer(self.depth.get_raw_depth_map(), np.uint16).reshape([self.depth.res[1],self.depth.res[0]])
+                        #self.depthIm8 = constrain(self.depthIm, self.constrain[0], self.constrain[1])
+
+                if self.color != []:
+                        #if self.colorIm != []:
+                        #       del self.colorIm
+                        #self.colorIm = np.frombuffer(self.color.get_synced_image_map_bgr(), np.uint8).reshape([self.color.res[1],self.color.res[0], 3])
+                        self.colorIm = np.frombuffer(self.color.get_raw_image_map_bgr(), np.uint8).reshape([self.color.res[1],self.color.res[0], 3])
 
 
+
+        def generateBackgroundModel(self):
+                # Get set of 5 frames and create background model
+                depthImgs = []
+                depthStackInd = 0
+                for i in xrange(5):
+                        ret = self.ctx.wait_one_update_all(self.depth)
+                        assert ret == None, "Error getting depth map"
+
+                        depthRawT = self.depth.get_tuple_depth_map()
+                        im = np.array(depthRawT).reshape([self.depth.res[1],self.depth.res[0]])
+                        depthImgs.append(im)
+                        time.sleep(.2)
+
+                depthImgs = np.dstack(depthImgs)
+
+                self.bgModel = getMeanImage(depthImgs)
+
+                self.bgModel8 = constrain(self.bgModel, self.constrain[0], self.constrain[1])
+                self.bgModel8[self.bgModel8==self.bgModel8.max()] = 0
+
+                # return self.depthIm
--- pyKinectTools/utils/RealtimeRecorder.py
+++ pyKinectTools/utils/RealtimeRecorder.py
@@ -1,63 +1,65 @@
 
-
 import os, time, sys
 import numpy as np
-import cv, cv2
+import scipy.misc as sm
+sys.path.append('/home/icu/kinect/pyKinectTools/build/lib.linux-i686-2.7')
 from pyKinectTools.utils.RealtimeReader import *
 
-dir_ = '/Users/colin/data/ICU_oct_test/'
 
-'''------------ Setup Kinect ------------'''
-depthConstraint = [500, 5000]
-''' Physical Kinect '''
-depthDevice = RealTimeDevice()
-depthDevice.addDepth(depthConstraint)
-depthDevice.addColor()
-depthDevice.start()
-depthDevice.setMaxDist(depthConstraint[1])
+def main():
+        dir_ = '/home/icu/data/icu_test/'
 
-''' Visualization '''
-cv2.namedWindow("Color")
-cv2.namedWindow("Depth")
+        '''------------ Setup Kinect ------------'''
+        depthConstraint = [500, 5000]
+        ''' Physical Kinect '''
+        depthDevice = RealTimeDevice()
+        depthDevice.addDepth(depthConstraint)
+        depthDevice.addColor()
+        depthDevice.start()
+        depthDevice.setMaxDist(depthConstraint[1])
 
-''' ------------- Main -------------- '''
 
-while 1:
-	depthDevice.update()
-	colorRaw = depthDevice.colorIm
-	depthRaw8 = depthDevice.depthIm8
+        ''' ------------- Main -------------- '''
 
-	time_ = time.localtime()
-	day = str(time_.tm_yday)
-	hour = str(time_.tm_hour)
-	minute = str(time_.tm_min)
-	second = str(time_.tm_sec)
-	ms = str(time.clock())
+        while 1:
+        #for i in range(100):
+                depthDevice.update()
+                colorRaw = depthDevice.colorIm
+                #depthRaw8 = depthDevice.depthIm8
+                depthRaw8 = depthDevice.depthIm
 
-	if depthRaw8 != []:
-		if not os.path.isdir(dir_+day):
-			os.mkdir(dir_+day)
+                time_ = time.localtime()
+                day = str(time_.tm_yday)
+                hour = str(time_.tm_hour)
+                minute = str(time_.tm_min)
+                second = str(time_.tm_sec)
+                ms = str(time.clock())
 
-		if not os.path.isdir(dir_+day+"/"+hour):
-			os.mkdir(dir_+day+"/"+hour)
+                if depthRaw8 != []:
+                        if not os.path.isdir(dir_+day):
+                                os.mkdir(dir_+day)
 
-		if not os.path.isdir(dir_+day+"/"+hour+"/"+minute):
-			os.mkdir(dir_+day+"/"+hour+"/"+minute)
-			os.mkdir(dir_+day+"/"+hour+"/"+minute+"/"+"depth")
-			os.mkdir(dir_+day+"/"+hour+"/"+minute+"/"+"color")
-			# os.mkdir(dir_+day+"/"+hour+"/"+minute+"/"+"skel")
+                        if not os.path.isdir(dir_+day+"/"+hour):
+                                os.mkdir(dir_+day+"/"+hour)
 
-		depthName = dir_+day+"/"+hour+"/"+minute+"/"+"depth"+"/"+\
-					"depth_"+day+"_"+hour+"_"+minute+"_"+second+"_"+ms
-		colorName = dir_+day+"/"+hour+"/"+minute+"/"+"color"+"/"+\
-					"color_"+day+"_"+hour+"_"+minute+"_"+second+"_"+ms
-		cv2.imwrite(depthName, depthRaw8)
-		cv2.imwrite(colorName, colorRaw)
+                        if not os.path.isdir(dir_+day+"/"+hour+"/"+minute):
+                                os.mkdir(dir_+day+"/"+hour+"/"+minute)
+                                os.mkdir(dir_+day+"/"+hour+"/"+minute+"/"+"depth")
+                                os.mkdir(dir_+day+"/"+hour+"/"+minute+"/"+"color")
+                                # os.mkdir(dir_+day+"/"+hour+"/"+minute+"/"+"skel")
 
-	print second
-	'''Display Image'''
-	# cv2.imshow("Depth", depthRaw8)
-	# cv2.imshow("Color", colorRaw)	
-	ret = cv2.waitKey(10)
-	if ret >= 0:
-		break
+
+                        depthName = dir_+day+"/"+hour+"/"+minute+"/"+"depth"+"/"+\
+                                                "depth_"+day+"_"+hour+"_"+minute+"_"+second+"_"+ms+".png"
+                        colorName = dir_+day+"/"+hour+"/"+minute+"/"+"color"+"/"+\
+                                                "color_"+day+"_"+hour+"_"+minute+"_"+second+"_"+ms+".jpg"
+
+                        sm.imsave(depthName, sm.imresize(depthRaw8, [240,320], 'nearest'))
+                        sm.imsave(colorName, sm.imresize(colorRaw, [240,320,3],'nearest'))
+
+
+                        print "Written", depthName
+                        depthRaw8 = None
+                        colorRaw = None
+
+                print second
